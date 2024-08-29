2f5c0e3f5b3e32f12bea7a0e84726cdfccb8d4dd
Cesar Hernandez <149346749+cesarurielhr@users.noreply.github.com>
2024-08-28 23:31:22 -0700

    create ej1.js

---
+++ ej1.js
@@ -0,0 +1,8 @@
+//1.Conteo de caracteres: Dada una cadena de texto y luego muestra el número de caracteres que contiene esa cadena.
+let texto = 'Hola';
+
+// Contar el número de caracteres en la cadena
+let nCaracteres = texto.length;
+
+// Mostrar resultado
+console.log('Número de caracteres encontrados: ' + nCaracteres);
